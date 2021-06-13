7a1fc9dc4e7a2bed44333b78f387df7b956aea91
Weikang01 <lwkdota@qq.com>
2021-06-13 03:06:53 -0400

    After Logical Device and Queues

--- tutorial/src/HelloTriangle.cpp
+++ tutorial/src/HelloTriangle.cpp
@@ -43,6 +43,13 @@ void DestroyDebugUtilsMessengerEXT(
 		func(instance, debugMessenger, pAllocator);
 }
 
+struct QueueFamilyIndices
+{
+	std::optional<uint32_t> graphicsFamily;
+
+	bool isComplete() { return graphicsFamily.has_value(); }
+};
+
 class HelloTriangleApplication
 {
 private:
@@ -50,12 +57,8 @@ private:
 	GLFWwindow* m_window;
 	VkDebugUtilsMessengerEXT m_debugMessenger;
 	VkPhysicalDevice m_physicalDevice = VK_NULL_HANDLE;
-	struct QueueFamilyIndices
-	{
-		std::optional<uint32_t> graphicsFamily;
-
-		bool isComplete() { return graphicsFamily.has_value(); }
-	};
+	VkDevice m_device;
+	VkQueue m_graphicsQueue;
 public:
 	void run()
 	{
@@ -225,6 +228,43 @@ private:
 
 #pragma endregion
 
+#pragma region logical device
+	void createLogicalDevice()
+	{
+		QueueFamilyIndices indices = findQueueFamilies(m_physicalDevice);
+		
+		VkDeviceQueueCreateInfo queueCreateInfo{};
+		queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+		queueCreateInfo.queueCount = 1;
+
+		float queuePriority = 1.f;
+		queueCreateInfo.pQueuePriorities = &queuePriority;
+
+		VkPhysicalDeviceFeatures deviceFeatures{};
+
+		VkDeviceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+		createInfo.pQueueCreateInfos = &queueCreateInfo;
+		createInfo.queueCreateInfoCount = 1;
+		createInfo.pEnabledFeatures = &deviceFeatures;
+		createInfo.enabledExtensionCount = 0;
+
+		if (enableValidationLayers)
+		{
+			createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+		}
+		else
+			createInfo.enabledLayerCount = 0;
+
+		if (vkCreateDevice(m_physicalDevice, &createInfo, nullptr, &m_device) != VK_SUCCESS)
+			throw std::runtime_error("failed to create logical device!");
+
+		vkGetDeviceQueue(m_device, indices.graphicsFamily.value(), 0, &m_graphicsQueue);
+	}
+#pragma endregion
+
 #pragma region general structure
 	void initWindow()
 	{
@@ -241,6 +281,7 @@ private:
 		createInstance();
 		setupDebugMessenger();
 		pickPhysicalDevice();
+		createLogicalDevice();
 	}
 
 	void mainLoop()
@@ -256,6 +297,7 @@ private:
 		if (enableValidationLayers)
 			DestroyDebugUtilsMessengerEXT(m_instance, m_debugMessenger, nullptr);
 
+		vkDestroyDevice(m_device, nullptr);
 		vkDestroyInstance(m_instance, nullptr);
 		glfwDestroyWindow(m_window);
 		glfwTerminate();
