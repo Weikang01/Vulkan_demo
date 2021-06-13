b708ad359083473d3de9de3a261174d557928568
Weikang01 <lwkdota@qq.com>
2021-06-12 20:13:20 -0400

    after project setup

---
+++ premake5.lua
@@ -0,0 +1,88 @@
+workspace "Vulkan_tutorial"
+    architecture ("x86_64")
+
+    startproject "tutorial"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+vulkan_sdk = "E:/VulkanSDK/1.2.176.1"
+
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["GLFW"] = "library/GLFW/include"
+IncludeDir["glm"] = "library/glm"
+IncludeDir["vulkan"] = vulkan_sdk .. "/include"
+
+LibDir = {}
+LibDir["vulkan"] = vulkan_sdk .. "/lib"
+
+include "library/GLFW"
+
+project "tutorial"
+    location "tutorial"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "On"
+
+    targetdir ("bin/".. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/".. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "%{prj.name}/src",
+        "%{IncludeDir.vulkan}",
+        "%{IncludeDir.GLFW}",
+        "%{IncludeDir.glm}"
+    }
+
+    libdirs
+    {
+        "%{LibDir.vulkan}"
+    }
+
+    links
+    {
+        "GLFW",
+        "vulkan-1"
+    }
+
+    filter "system:windows"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "WIN32"
+        }
+
+    filter "configurations:Debug"
+        defines 
+        {
+            "_DEBUG",
+            "_CONSOLE",
+            "_LIB"
+        }
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines 
+        {
+            "NDEBUG",
+            "_CONSOLE",
+            "_LIB"
+        }
+        runtime "Release"
+        optimize "On"
